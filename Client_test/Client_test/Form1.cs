aa348604af6aa121dab3b7ad4fcd6c49209ce68f
totoro11 <tjdalsdlaek1@naver.com>
2020-12-21 17:02:27 +0900

    21일차 계산식 출력분까지 커밋입니다

---
+++ Client_test/Client_test/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using System.Net;
+
+namespace Client_test
+{
+    public partial class clacWiondow : Form
+    {
+
+        private Socket client_socket;   //클라이언트 소켓 선언
+        private bool isConnected;   //참-거짓 값을 이용하여 통신 오류 발생 시 대처
+        private byte[] bytes = new byte[1024];
+        private const int PORT = 8087;
+        private string clientMSG = "";
+
+
+        public clacWiondow()
+        {
+            InitializeComponent();
+            isConnected = false;    //통신 시작되면 true
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //connect 버튼 클릭 이벤트
+            try
+            {
+                //소켓생성
+                start();
+
+                //스레드 생성
+                Thread listen_thread = new Thread(do_receive);
+
+                //스레드시작
+                listen_thread.Start();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+
+            /*소켓이 연결이 잘 되면
+             1. 버튼 disconnect로 바뀜 / ip,port 입력창 비활성화
+             2. connSatate = CONNECTION
+             3. 계산식 입력창 활성화
+            이미 연결되어있는 상태일때 disconnect버튼을 누르면 소켓 연결해제
+             */
+            if (client_socket.Connected)
+            {
+                btnConnect.Text = "DISCONNECT";
+                ipTextBox.ReadOnly = true;
+                portTextBox.ReadOnly = true;
+                connectState.Text = "CONNECTION";
+                connectState.ForeColor = Color.Blue;
+                inputCalcTextBox.ReadOnly = false;
+            }
+            else
+            {
+                btnConnect.Text = "CONNECT";
+                ipTextBox.ReadOnly = false;
+                portTextBox.ReadOnly = false;
+                connectState.Text = "UNCONNECTION";
+                connectState.ForeColor = Color.Red;
+                inputCalcTextBox.ReadOnly = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text = "";
+        }
+
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "8";
+        }
+
+        private void iptextBox_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        private void btnEqualSign_Click(object sender, EventArgs e)
+        {
+            // 트리 버튼 클릭시 이벤트 함수
+            try
+            {
+                //소켓 연결안되있을때는 무시
+                if (isConnected == false) return;
+
+                //UTF8로 인코딩 상대에게 Client : 데이터 + "<eof>" 송신
+                byte[] msg = Encoding.UTF8.GetBytes(" Client : " + inputCalcTextBox.Text + "<eof>");
+
+                //계산기 = 버튼을 이용해서 내용 송신
+                int bytesSent = client_socket.Send(msg);
+
+                //connStateListBox에 Client : 내가 보낸 데이터 출력
+                connStateListBox.Items.Add(" Client : " + inputCalcTextBox.Text);
+                inputCalcTextBox.Clear();
+                inputCalcTextBox.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connStateListBox.SelectedIndex = connStateListBox.Items.Count - 1;
+            }
+
+            treeView1.Nodes.Clear();    // Data Initialized
+            String data2 = clientMSG;        // Data Insert
+            String[] str = data2.Split('#');    // Data Split
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                // If Operand
+                if (!(str[i].Equals("+") || str[i].Equals("-") || str[i].Equals("*") || str[i].Equals("/")))
+                {
+                    TreeNode node = new TreeNode(str[i]);
+                    stack.Push(node);
+                }
+                // If Operation
+                else
+                {
+                    TreeNode left = stack.Pop();
+                    TreeNode right = stack.Pop();
+                    TreeNode node = new TreeNode(str[i]);
+                    node.Nodes.Add(left);
+                    node.Nodes.Add(right);
+                    stack.Push(node);
+                }
+            }
+
+            treeView1.Nodes.Add(stack.Pop());   // TreeView <- Top of stack
+            treeView1.ExpandAll();
+            clientMSG = "";
+        }
+
+
+
+
+        public void start()
+        {
+            //소켓 생성함수
+            try
+            {
+                //소켓 연결되있으면 무시
+                if (isConnected) return;
+
+                //소켓 연결안되있으면 소켓 생성 시도
+                client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                client_socket.Connect(new IPEndPoint(IPAddress.Parse(ipTextBox.Text), PORT));
+
+                //만약 소켓이 연결되면 소켓 연결됨을 보여줌
+                //소켓 생성이 제대로 안되면 소켓 닫아주기
+                if (client_socket.Connected)
+                {
+                    connStateListBox.Items.Add(String.Format("소켓 연결되었습니다 ", client_socket.RemoteEndPoint.ToString()));
+                    isConnected = true;
+                }
+                else
+                {
+                    client_socket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                connStateListBox.SelectedIndex = connStateListBox.Items.Count - 1;
+            }
+        }
+
+        void do_receive()
+        {
+            while (isConnected) //무한루프 설정
+            {
+                try
+                {
+                    while (true)    //통신이 시작되었을때
+                    {
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = client_socket.Receive(bytes);
+                        clientMSG += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                        //<eof>가 있으면 브레이크
+                        if (clientMSG.IndexOf("<eof>") > -1) break;
+                    }
+                    clientMSG = clientMSG.Substring(0, clientMSG.Length - 5);
+                    bool ST = clientMSG.Contains("CC##");
+                    CheckForIllegalCrossThreadCalls = false;
+
+                    // ? 없으면 크로스스레드 오류 발생 (https://blog.naver.com/spb02293/221671835503 참조)
+                    Invoke((MethodInvoker)delegate
+                    {
+                        connStateListBox.Items.Add(clientMSG);
+                        if (ST == true)
+                        {
+                            // treeListBox.Items.Add(" c# 명령어 값 전달");
+                        }
+                    }
+                    );
+                    // clientMSG = "";
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                    MessageBox.Show(ex.ToString());
+
+                    isConnected = false;
+                    while (isConnected == false)
+                    {
+                        start();
+                        Thread listen_thread = new Thread(do_receive);
+                        listen_thread.Start();
+                    }
+                }
+                finally
+                {
+                    connStateListBox.SelectedIndex = connStateListBox.Items.Count - 1;
+                }
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "2";
+        }
+
+        private void btn3_Click_1(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "3";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "0";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "7";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "9";
+        }
+
+        private void btnPeriod_Click(object sender, EventArgs e)
+        {
+            if (inputCalcTextBox.Text.Contains("."))
+                return;
+            else
+                inputCalcTextBox.Text += ".";
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "+";
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "-";
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "*";
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "/";
+        }
+
+        private void bracket1_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += "(";
+        }
+
+        private void bracket2_Click(object sender, EventArgs e)
+        {
+            inputCalcTextBox.Text += ")";
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SureProjectC/SureProjectC/SureProjectC/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace SureProjectC
+{
+    public partial class Form1 : Form
+    {
+        Socket listen_socket;
+        Socket client_socket;
+        bool isConnected;
+        byte[] bytes = new Byte[1024];
+        static string data;
+
+        static string input = "";
+        static char[] stack = new char[100];   // 스택
+        static int point = 0;                  // 스택 포인트
+        static string send_mmsg = "";             // 출력 문자열 (postfix) (out put)
+
+        public Form1()
+        {
+            InitializeComponent();
+            isConnected = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            start("192.168.121.140", 8087, 10);   //(url, port, 백로그 보관요청 최대수)
+        }
+
+        public void start(string host, int port, int backlog)
+        {
+            this.listen_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address;
+            if (host == "0.0.0.0")
+            {
+                address = IPAddress.Any;
+            }
+            else
+            {
+                address = IPAddress.Parse(host);    //위의 host값 가져옴
+            }
+            IPEndPoint endpoint = new IPEndPoint(address, port);
+            try
+            {
+                listen_socket.Bind(endpoint);
+                listen_socket.Listen(backlog);
+
+                client_socket = listen_socket.Accept();
+                listBox1.Items.Add("연결 시작");
+                isConnected = true;
+                Thread listen_thread = new Thread(do_receive);
+                listen_thread.Start();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+        private void SendCal()
+        {
+            try
+            {
+                if (isConnected == false) return;
+                byte[] msg = Encoding.UTF8.GetBytes(send_mmsg + "<eof>");
+                int bytesSent = client_socket.Send(msg);
+                send_mmsg = "";
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            }
+        }
+
+        void do_receive()
+        {
+            while (isConnected)
+            {
+                try
+                {
+                    while (true)
+                    {
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = client_socket.Receive(bytes);
+                        data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<eof>") > -1) break;
+                    }
+                    data = data.Substring(0, data.Length - 5);
+                    CheckForIllegalCrossThreadCalls = false;
+                    Invoke((MethodInvoker)delegate
+                    {
+                        listBox1.Items.Add(data);
+                        if (data.Contains("C#"))
+                        {
+                            listBox1.Items.Add(" 후위표기 값 클라이언트에게 전달 ");
+                            byte[] mmsg = Encoding.UTF8.GetBytes(" OP : CC## " + "<eof>");
+                            int bytesSent = client_socket.Send(mmsg);
+                        }
+                        //send_mmsg = data;
+                        Calculation();
+                        SendCal();
+                    }
+                    );
+                    data = "";
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                }
+                catch (Exception ex)
+                {
+                    ex.ToString();
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private static void Calculation()
+        {
+            Console.Out.Write("infix 수식을 입력하시오 : ");
+            input = Console.In.ReadLine();      // 입력
+            Console.Out.WriteLine("입력하신 infix 수식 : " + input + "\n");
+
+            for (int i = 0; i < data.Length; i++)  // 한글자씩 반복하기
+            {
+                char c = data[i];                  // 한글자씩 가져오기
+
+                if (c >= '0' && c <= '9')           // 피연산자의 경우
+                {
+                    send_mmsg += c.ToString();
+                }
+                else if (c == '+' || c == '-')
+                {
+                    while (!Stack_IsEmpty())
+                    {
+                        // 기존 스택이 비어있지 않다면
+                        char prev_c = Stack_Pop();  // 스택 최상단의 연산자를 가져옴
+                        if (prev_c == '*' || prev_c == '/' || prev_c == '+' || prev_c == '-')
+                        {
+                            // 스택 최상단 연산자가 현재 연산자 보다 상위 연산자라면
+                            send_mmsg += prev_c.ToString();
+                        }
+                        else
+                        {
+                            Stack_Push(prev_c);
+                            break;
+                        }
+                    }
+                    Stack_Push(c);
+                }
+                else if (c == '*' || c == '/')
+                {
+                    while (!Stack_IsEmpty())
+                    {
+                        // 기존 스택이 비어있지 않다면
+                        char prev_c = Stack_Pop();  // 스택 최상단의 연산자를 가져옴
+                        if (prev_c == '*' || prev_c == '/')
+                        {
+                            // 스택 최상단 연산자가 현재 연산자 보다 상위 연산자라면
+                            send_mmsg += prev_c.ToString();
+                        }
+                        else
+                        {
+                            Stack_Push(prev_c);
+                            break;
+                        }
+                    }
+                    Stack_Push(c);
+                }
+                else if (c == '(')
+                {
+                    Stack_Push(c);
+                }
+                else if (c == ')')
+                {
+                    send_mmsg += "#";
+                    while (true)
+                    {
+                        char prev_oper = Stack_Pop();   // 기존 스택의 최상위 연산자를 꺼내온다. 
+                        if (prev_oper == '(')
+                            break;
+                        send_mmsg += prev_oper.ToString(); // 기존 스택의 최상위 연산자를 출력으로 빼낸다. 
+                    }
+                }
+            }
+
+            // stack에 있는 모든 연산자를 순차적으로 꺼내서 output에 넣는다. 
+            while (!Stack_IsEmpty())
+            {
+                send_mmsg += Stack_Pop().ToString();
+            }
+
+            // 결과 출력
+           // Console.Out.WriteLine();
+            Stack_Show();
+            //Console.Out.WriteLine("Postfix : " + send_mmsg);
+
+            //Console.Out.Write("아무키나 입력하시오.");
+            //Console.In.ReadLine();  // 종료전 일시 정지
+        }
+
+        private static void Stack_Show()
+        {
+            // 스택 내용을 모두 보여준다. 
+            for (int i = 0; i < point; i++)
+            {
+                Console.Out.Write(stack[i]);
+            }
+            Console.Out.WriteLine();
+        }
+
+        private static void Stack_Push(char c)
+        {
+            // 스택의 최상위에 값을 추가한다. 
+            stack[point] = c;
+            point++;
+            return;
+        }
+
+        private static char Stack_Pop()
+        {
+            // 스택의 최상위에 값을 가져온다. 
+            point--;
+            char result = stack[point];
+            stack[point] = '\0';
+            return result;
+        }
+
+        private static bool Stack_IsEmpty()
+        {
+            // 스택이 비었다면 true를 비어있지 않다면 false를 반환한다. 
+            send_mmsg += "#";
+            return (point == 0 ? true : false);
+        }
+    }
+}
